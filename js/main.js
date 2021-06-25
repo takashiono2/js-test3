c3c8a72aa50151330c1c8f5a02e782bb86e6f599
takashiono2 <takashiono710@gmail.com>
2021-06-26 00:20:41 +0900

    要素の複製、挿入してみよう

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 'use strict';
 {
   document.querySelector('button').addEventListener('click',()=>{
-    const item2 = document.createElement('li')
-    item2.textContent = 'item 2';
-    // const ulNode = document.querySelector('ul')
-    const ul = document.querySelector('ul')
-    ul.appendChild(item2);
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
+
+    const ul = document.querySelector('ul');
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2);
   });
 }
+
+// #10
+// {
+//   document.querySelector('button').addEventListener('click',()=>{
+//     const item2 = document.createElement('li')
+//     item2.textContent = 'item 2';
+//     // const ulNode = document.querySelector('ul')
+//     const ul = document.querySelector('ul')
+//     ul.appendChild(item2);
+//   });
+// }
 
 // #9
 // {
