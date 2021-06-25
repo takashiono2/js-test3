714109f71e1b768d6ce1584e45371da68b7f844a
takashiono2 <takashiono710@gmail.com>
2021-06-26 01:43:56 +0900

    input要素を操作してみよう

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 'use strict';
 {
   document.querySelector('button').addEventListener('click',()=>{
-    const item1 = document.querySelectorAll('li')[1];
-    // item1.remove();
-    document.querySelector('ul').removeChild(item1);
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
+    text.value = '';
+    text.focus();
   });
 }
+
+// #12
+// {
+//   document.querySelector('button').addEventListener('click',()=>{
+//     const item1 = document.querySelectorAll('li')[1];
+//     // item1.remove();
+//     document.querySelector('ul').removeChild(item1);
+//   });
+// }
 
 // #11
 // {
