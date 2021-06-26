1d0cd42bec4e2386677f8bc71febfd3d269e7501
takashiono2 <takashiono710@gmail.com>
2021-06-26 13:03:22 +0900

    チェックボックスを操作してみよう

--- js/main.js
+++ js/main.js
@@ -3,17 +3,34 @@
 {
   document.querySelector('button').addEventListener('click',()=>{
     const colors = document.querySelectorAll('input');
-    let selectColor;
-    colors.forEach(color =>{
-      if(color.checked === true){
-        selectColor = color.value;
+    const selectedColors = [];
+    colors.forEach(color => {
+      if (color.checked === true) {
+        selectedColors.push(color.value);
       }
     });
-    const li = document.createElement('li');
-    li.textContent = selectColor
-    document.querySelector('ul').appendChild(li);
-  });
+      const li = document.createElement('li');
+      // li.textContent = selectedColors.join(',');
+      li.textContent = selectedColors;
+      document.querySelector('ul').appendChild(li);
+    });
 }
+
+// #15
+// {
+//   document.querySelector('button').addEventListener('click',()=>{
+//     const colors = document.querySelectorAll('input');
+//     let selectColor;
+//     colors.forEach(color =>{
+//       if(color.checked === true){
+//         selectColor = color.value;
+//       }
+//     });
+//     const li = document.createElement('li');
+//     li.textContent = selectColor
+//     document.querySelector('ul').appendChild(li);
+//   });
+// }
 
 // #14
 // {
