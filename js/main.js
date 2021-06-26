12120d37e8798e1f54d117ed46171a38c50353aa
takashiono2 <takashiono710@gmail.com>
2021-06-26 17:21:49 +0900

    いろいろなイベントを見てみよう

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click',()=>{
-    const colors = document.querySelectorAll('input');
-    const selectedColors = [];
-    colors.forEach(color => {
-      if (color.checked === true) {
-        selectedColors.push(color.value);
-      }
-    });
-      const li = document.createElement('li');
-      // li.textContent = selectedColors.join(',');
-      li.textContent = selectedColors;
-      document.querySelector('ul').appendChild(li);
-    });
+  document.querySelector('button').addEventListener('dblclick',()=>{
+    console.log('Double Cliked！');
+  });
+  document.addEventListener('mousemove',()=>{
+    console.log('moved!');
+  });
 }
+
+// #16
+// {
+//   document.querySelector('button').addEventListener('click',()=>{
+//     const colors = document.querySelectorAll('input');
+//     const selectedColors = [];
+//     colors.forEach(color => {
+//       if (color.checked === true) {
+//         selectedColors.push(color.value);
+//       }
+//     });
+//       const li = document.createElement('li');
+//       // li.textContent = selectedColors.join(',');
+//       li.textContent = selectedColors;
+//       document.querySelector('ul').appendChild(li);
+//     });
+// }
 
 // #15
 // {
